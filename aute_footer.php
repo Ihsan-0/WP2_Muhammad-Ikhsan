b98a748972f469de801e036b9dcc450e28e60048
ikhsan71 <78256229+ikhsan71@users.noreply.github.com>
2021-11-13 23:02:06 +0700

    Tugas WevProg II Pertemuan 9

---
+++ aute_footer.php
@@ -0,0 +1,16 @@
+<!-- Bootstrap core Javascript-->
+<script src="<?= base_url('assets/'); ?>vendor/jquery.min.js"></script>
+<script src="<?= base_url('assets/'); ?>vendor/jquery/js/bootstrap.bundle.min.js"></script>
+
+<!-- Core plugin Javascript-->
+<script src="<?= base_url('assets/'); ?>vendor/jquery-easing/jquery.easing.min.js"></script>
+
+<!-- Custom scripts for all pages-->
+<script src="<?= base_url('assets/'); ?>js/sb-admin-2.min.js"></script>
+<script>
+    $('.alert-message').alert().delay(3000).slideup('slow');
+</script>
+
+</body>
+
+</html>
